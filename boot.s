bd92545b36c007ab75fb54c4d29894c6b08efec9
Giuseppe Barletta <giusebobo@gmail.com>
2022-06-04 18:22:48 +0200

    Indentation fix

--- boot.s
+++ boot.s
@@ -6,10 +6,10 @@
 
 entry:
     csrr    t0, mhartid
-	bnez    t0, loop        # if not hart 0, loop forever
+    bnez    t0, loop        # if not hart 0, loop forever
     la      sp, stack_top   # set up stack pointer (see link.ld)
     jal     kmain
 
 loop:
-	wfi
-	j       loop
+    wfi
+    j       loop
